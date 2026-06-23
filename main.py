9d2f15ccac0cd0445449e4ba01cd8ed422d174fd
Pkab1972 <pkab1972@gmail.com>
2026-06-23 17:09:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ API_ID = 33309973
 API_HASH = "3e4359da0ec8656b02e28beeca07a0ca"
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
 TARGET = -4361831458
+SOURCE = 1757874218
 
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
-@client.on(events.NewMessage())
+@client.on(events.NewMessage(chats=SOURCE))
 async def handler(event):
-    print(f"Received from {event.chat_id}: {event.raw_text}")
+    print(f"InPlayGuru alert received!")
     await client.send_message(TARGET, event.raw_text)
-    print(f"Sent!")
+    print(f"Sent to Staging2!")
 
 client.start()
 client.run_until_disconnected()
