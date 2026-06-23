350ccc2103afb7d3a194ddb57eb696ecdbc2f5b1
Pkab1972 <pkab1972@gmail.com>
2026-06-23 16:46:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ from telethon.sessions import StringSession
 API_ID = 33309973
 API_HASH = "3e4359da0ec8656b02e28beeca07a0ca"
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
-TARGET = -1004361831458
 
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
 @client.on(events.NewMessage())
 async def handler(event):
-    print(f"Received from {event.chat_id}")
-    await client.forward_messages(TARGET, event.message)
-    print(f"Forwarded!")
+    print(f"chat_id: {event.chat_id} | peer_id: {event.message.peer_id}")
 
 client.start()
 client.run_until_disconnected()
