92849e0f9dfd7926de5301809353327d63940790
Pkab1972 <pkab1972@gmail.com>
2026-06-24 09:18:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from telethon.sessions import StringSession
 API_ID = 33309973
 API_HASH = "3e4359da0ec8656b02e28beeca07a0ca"
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
-TARGET = -4361831458
-SOURCE = 1757874218
+TARGET = "https://hook.eu1.make.com/co2ti6ci13jkt1hm75x2gn6dv1fetzva"
+SOURCE = -5514769696
 
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
 @client.on(events.NewMessage(chats=SOURCE))
 async def handler(event):
     print(f"InPlayGuru alert received!")
-    await client.send_message(TARGET, event.raw_text)
-    print(f"Sent to Staging2!")
+    import aiohttp
+    async with aiohttp.ClientSession() as session:
+        await session.post(TARGET, data={"text": event.raw_text})
+    print(f"Sent to Make.com!")
 
 client.start()
 client.run_until_disconnected()
